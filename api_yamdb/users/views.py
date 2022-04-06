7fd9b556c497a03df3636d9759677062378f5b73
Кузнецов Георгий <ikuz57@gmail.com>
2022-04-06 17:58:59 +0800

    добавил комментарий

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -53,6 +53,8 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     lookup_field = 'username'
 
+    # метод get_permissions оставлен,
+    # так как без него не проходят тесты
     def get_permissions(self):
 
         if self.action in [
